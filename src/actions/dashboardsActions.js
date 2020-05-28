27e2053e3755ce2011552eaa1304fa2b15302b24
Maxim Antonov <helloto@me.com>
2020-05-28 16:07:04 +0300

    Use dashboard number in URL

--- src/actions/dashboardsActions.js
+++ src/actions/dashboardsActions.js
@@ -1,135 +1,141 @@
-import Registry from '../WidgetsRegistry'
-import _ from 'lodash'
-import { subscribeToApi, unsubscribeFromApi } from './apiActions'
+import Registry from "../WidgetsRegistry";
+import _ from "lodash";
+import { subscribeToApi, unsubscribeFromApi } from "./apiActions";
 
-export const SET_DASHBOARDS = 'SET_DASHBOARDS'
-export const SET_CURRENT_DASHBOARD = 'SET_CURRENT_DASHBOARD'
-export const DASHBOARDS_PLAY = 'DASHBOARDS_PLAY'
-export const DASHBOARDS_PAUSE = 'DASHBOARDS_PAUSE'
+export const SET_DASHBOARDS = "SET_DASHBOARDS";
+export const SET_CURRENT_DASHBOARD = "SET_CURRENT_DASHBOARD";
+export const DASHBOARDS_PLAY = "DASHBOARDS_PLAY";
+export const DASHBOARDS_PAUSE = "DASHBOARDS_PAUSE";
 
-const ignoreProps = ['extension', 'widget', 'x', 'y', 'columns', 'rows']
+const ignoreProps = ["extension", "widget", "x", "y", "columns", "rows"];
 
-let timer
+let timer;
 
-const SECOND = 1000
+const SECOND = 1000;
 
-export const setDashboards = dashboards => {
-    return (dispatch, getState) => {
-        const { api } = getState()
+export const setDashboards = (dashboards) => {
+  return (dispatch, getState) => {
+    const { api } = getState();
 
-        const currentSubscriptionsIds = api
-            .get('subscriptions')
-            .keySeq()
-            .toArray()
-        const newSubscriptionsIds = []
+    const currentSubscriptionsIds = api.get("subscriptions").keySeq().toArray();
+    const newSubscriptionsIds = [];
 
-        dashboards.forEach((dashboard, dahsboardIndex) => {
-            dashboard.widgets.forEach((w, widgetIndex) => {
-                if (Registry.has(w.extension, w.widget)) {
-                    const component = Registry.getComponent(w.extension, w.widget)
+    dashboards.forEach((dashboard, dahsboardIndex) => {
+      dashboard.widgets.forEach((w, widgetIndex) => {
+        if (Registry.has(w.extension, w.widget)) {
+          const component = Registry.getComponent(w.extension, w.widget);
 
-                    if (typeof component.getApiRequest === 'function') {
-                        const childProps = _.omit(w, ignoreProps)
-                        const subscription = component.getApiRequest(childProps)
+          if (typeof component.getApiRequest === "function") {
+            const childProps = _.omit(w, ignoreProps);
+            const subscription = component.getApiRequest(childProps);
 
-                        if (!_.isObject(subscription) || !subscription.id) {
-                            // eslint-disable-next-line no-console
-                            console.error(
-                                `widget ${w.extension}.${
-                                    w.widget
-                                } 'getApiRequest()' must return an object with an 'id' property`
-                            )
-                        } else {
-                            dashboards[dahsboardIndex].widgets[widgetIndex].subscriptionId =
-                                subscription.id
-                            dispatch(subscribeToApi(subscription))
-                            newSubscriptionsIds.push(subscription.id)
-                        }
-                    }
-                }
-            })
-        })
+            if (!_.isObject(subscription) || !subscription.id) {
+              // eslint-disable-next-line no-console
+              console.error(
+                `widget ${w.extension}.${w.widget} 'getApiRequest()' must return an object with an 'id' property`
+              );
+            } else {
+              dashboards[dahsboardIndex].widgets[widgetIndex].subscriptionId =
+                subscription.id;
+              dispatch(subscribeToApi(subscription));
+              newSubscriptionsIds.push(subscription.id);
+            }
+          }
+        }
+      });
+    });
 
-        const staleSubscriptionsIds = _.difference(currentSubscriptionsIds, newSubscriptionsIds)
-        staleSubscriptionsIds.forEach(id => {
-            dispatch(unsubscribeFromApi(id))
-        })
+    const staleSubscriptionsIds = _.difference(
+      currentSubscriptionsIds,
+      newSubscriptionsIds
+    );
+    staleSubscriptionsIds.forEach((id) => {
+      dispatch(unsubscribeFromApi(id));
+    });
 
-        dispatch({ type: SET_DASHBOARDS, dashboards })
-    }
-}
+    dispatch({ type: SET_DASHBOARDS, dashboards });
+  };
+};
 
-const setCurrentDashboard = index => {
-    return {
-        type: SET_CURRENT_DASHBOARD,
-        index,
-    }
-}
+const setCurrentDashboard = (index) => {
+  history.pushState(
+    {
+      id: "dashboard",
+    },
+    "",
+    window.location.origin + "/" + (index + 1)
+  );
+
+  return {
+    type: SET_CURRENT_DASHBOARD,
+    index,
+  };
+};
 
 export const previous = () => {
-    return (dispatch, getState) => {
-        const {
-            dashboards: { dashboards, current },
-        } = getState()
+  return (dispatch, getState) => {
+    const {
+      dashboards: { dashboards, current },
+    } = getState();
 
-        let prevIndex
-        if (current > 0) {
-            prevIndex = current - 1
-        } else {
-            prevIndex = dashboards.length - 1
-        }
-
-        dispatch(setCurrentDashboard(prevIndex))
+    let prevIndex;
+    if (current > 0) {
+      prevIndex = current - 1;
+    } else {
+      prevIndex = dashboards.length - 1;
     }
-}
+
+    dispatch(setCurrentDashboard(prevIndex));
+  };
+};
 
 export const next = () => {
-    return (dispatch, getState) => {
-        const {
-            configuration: { configuration },
-            dashboards: { dashboards, current, isPlaying },
-        } = getState()
-        const { rotationDuration } = configuration
+  return (dispatch, getState) => {
+    const {
+      configuration: { configuration },
+      dashboards: { dashboards, current, isPlaying },
+    } = getState();
+    const { rotationDuration } = configuration;
 
-        let nextIndex
-        if (current < dashboards.length - 1) {
-            nextIndex = current + 1
-        } else {
-            nextIndex = 0
-        }
-
-        dispatch(setCurrentDashboard(nextIndex))
-
-        if (isPlaying) {
-            clearTimeout(timer)
-            timer = setTimeout(() => {
-                dispatch(next())
-            }, Number(rotationDuration) * SECOND)
-        }
+    let nextIndex;
+    if (current < dashboards.length - 1) {
+      nextIndex = current + 1;
+    } else {
+      nextIndex = 0;
     }
-}
+
+    dispatch(setCurrentDashboard(nextIndex));
+
+    if (isPlaying) {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        dispatch(next());
+      }, Number(rotationDuration) * SECOND);
+    }
+  };
+};
 
 export const play = () => {
-    clearTimeout(timer)
+  clearTimeout(timer);
 
-    return (dispatch, getState) => {
-        const {
-            configuration: { configuration },
-            dashboards: { dashboards },
-        } = getState()
-        const { rotationDuration } = configuration
+  return (dispatch, getState) => {
+    const {
+      configuration: { configuration },
+      dashboards: { dashboards },
+    } = getState();
+    const { rotationDuration } = configuration;
 
-        if (dashboards.length > 1) {
-            dispatch({ type: DASHBOARDS_PLAY })
-            timer = setTimeout(() => {
-                dispatch(next())
-            }, Number(rotationDuration) * SECOND)
-        }
+    if (dashboards.length > 1) {
+      dispatch({ type: DASHBOARDS_PLAY });
+      timer = setTimeout(() => {
+        dispatch(next());
+      }, Number(rotationDuration) * SECOND);
     }
-}
+  };
+};
 
 export const pause = () => {
-    clearTimeout(timer)
+  clearTimeout(timer);
 
-    return { type: DASHBOARDS_PAUSE }
-}
+  return { type: DASHBOARDS_PAUSE };
+};
--- src/reducers/dashboardsReducer.js
+++ src/reducers/dashboardsReducer.js
@@ -1,44 +1,52 @@
 import {
-    SET_DASHBOARDS,
-    SET_CURRENT_DASHBOARD,
-    DASHBOARDS_PLAY,
-    DASHBOARDS_PAUSE,
-} from '../actions/dashboardsActions'
+  SET_DASHBOARDS,
+  SET_CURRENT_DASHBOARD,
+  DASHBOARDS_PLAY,
+  DASHBOARDS_PAUSE,
+} from "../actions/dashboardsActions";
 
 export default function dashboards(
-    state = {
-        dashboards: [],
-        current: 0,
-        isPlaying: false,
-    },
-    action
+  state = {
+    dashboards: [],
+    current: (() => {
+      const predefinedDashboard = parseInt(
+        window.location.pathname.replace("/", ""),
+        10
+      );
+      if (predefinedDashboard > 0 && predefinedDashboard <= 10) {
+        return predefinedDashboard - 1;
+      } else return 0;
+    })(),
+    isPlaying: false,
+  },
+  action
 ) {
-    switch (action.type) {
-        case SET_DASHBOARDS:
-            return {
-                ...state,
-                dashboards: action.dashboards,
-            }
+  switch (action.type) {
+    case SET_DASHBOARDS:
+      return {
+        ...state,
+        dashboards: action.dashboards,
+      };
 
-        case SET_CURRENT_DASHBOARD:
-            return {
-                ...state,
-                current: action.index,
-            }
+    case SET_CURRENT_DASHBOARD:
+      return {
+        ...state,
+        current: action.index,
+      };
 
-        case DASHBOARDS_PLAY:
-            return {
-                ...state,
-                isPlaying: true,
-            }
+    case DASHBOARDS_PLAY:
+      return {
+        ...state,
+        isPlaying: true,
+      };
 
-        case DASHBOARDS_PAUSE:
-            return {
-                ...state,
-                isPlaying: false,
-            }
+    case DASHBOARDS_PAUSE:
+      return {
+        ...state,
+        isPlaying: false,
+      };
 
-        default:
-            return state
-    }
+    default:
+      return state;
+  }
 }
